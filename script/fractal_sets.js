17dc9ecd54cf8b7a906a6c241fabfa4a033897b8
SamuRodDev <132297110+SamuRodDev@users.noreply.github.com>
2023-06-27 18:23:45 +0200

    [fix]Arreglo bug parpadeo punto rojo

--- script/fractal_sets.js
+++ script/fractal_sets.js
@@ -9,14 +9,19 @@ const canvasJul = $('#canvasJul');
 
 // Variables para el dibujo del set de Mandelbrot y del set de Julia
 const default_complex = new Complex(0, 0);
-const pointRadius = 5;
-const canvasBorder = (canvasMandelbrot[0].offsetWidth - canvasMandelbrot[0].width) / 2;
-let dragging = false;
-let mouseX = 0;
-let mouseY = 0; 
+const redPointOrigin = complexToPixel(default_complex, pointCanvas);
 let juliaComplex = default_complex;
 let sendX = 0;
 let sendY = 0;
+
+//Definición propiedades punto rojo
+const redPoint = {
+    x: redPointOrigin.real,
+    y: redPointOrigin.imaginary,
+    radius: 5,
+    color: 'red',
+    isDragging: false,
+  };
 
 // Obtener el contexto de los canvas
 const ctxMandelbrot = canvasMandelbrot.get(0).getContext('2d');
@@ -25,52 +30,31 @@ const ctxJulia = canvasJul.get(0).getContext('2d');
 
 // Controlador de eventos para cuando se presiona el botón del mouse
 pointCanvas.on('mousedown', function(event) {
-    dragging = true;
-    mouseX = event.offsetX;
-    mouseY = event.offsetY;
-  });
+    const rect = pointCanvas[0].getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    if (
+        mouseX >= redPoint.x - redPoint.radius &&
+        mouseX <= redPoint.x + redPoint.radius &&
+        mouseY >= redPoint.y - redPoint.radius &&
+        mouseY <= redPoint.y + redPoint.radius
+      ) {
+        redPoint.isDragging = true;
+      }
+});
   
 // Controlador de eventos para cuando se mueve el mouse
 pointCanvas.on('mousemove', function(event) {
     // Verifica si se está arrastrando el punto
-    if (dragging) {
-        // Calcula el desplazamiento en el eje X y en el eje Y
-        const deltaX = event.offsetX - mouseX;
-        const deltaY = event.offsetY - mouseY;
-
-        // Obtiene la posición actual del punto en el canvas
-        const oldX = parseFloat(pointCanvas.attr('data-x'));
-        const oldY = parseFloat(pointCanvas.attr('data-y'));
-
-        // Calcula los límites del canvas para evitar que el punto salga de la zona visible
-        const centerAux = complexToPixel(default_complex, canvasMandelbrot);
-        const leftAux = 0 - centerAux.real + pointRadius;
-        const rightAux = canvasMandelbrot[0].width - centerAux.real - pointRadius;
-        const topAux = 0 - centerAux.imaginary + pointRadius;
-        const bottomAux = canvasMandelbrot[0].height - centerAux.imaginary - pointRadius;
-
-        // Calcula las nuevas coordenadas del punto
-        let newX = oldX + deltaX;
-        let newY = oldY + deltaY;
-
-        // Verifica si las nuevas coordenadas exceden los límites del canvas y las ajusta en caso necesario
-        if (newX < leftAux) newX = leftAux;
-        if (newX > rightAux) newX = rightAux;
-        if (newY < topAux) newY = topAux;
-        if (newY > bottomAux) newY = bottomAux;
-
-        // Actualiza la posición del punto en el canvas
-        pointCanvas.css({left: newX + 'px', top: newY + 'px'});
-        pointCanvas.attr('data-x', newX);
-        pointCanvas.attr('data-y', newY);
-
-        // Actualiza las coordenadas del mouse
-        mouseX = event.offsetX;
-        mouseY = event.offsetY;
+    if (redPoint.isDragging) {
+        const rect = pointCanvas[0].getBoundingClientRect();
+        redPoint.x = event.clientX - rect.left;
+        redPoint.y = event.clientY - rect.top;
 
         // Calcula las coordenadas complejas correspondientes al punto actual
-        let x = parseInt(newX + centerAux.real);
-        let y = parseInt(newY + centerAux.imaginary);
+        let x = parseInt(redPoint.x);
+        let y = parseInt(redPoint.y);
         let zAux1 = pixelToComplex({x, y}, pointCanvas);
         let zAux2 = new Complex(zAux1.real.toFixed(2), zAux1.imaginary.toFixed(2));
 
@@ -83,14 +67,14 @@ pointCanvas.on('mousemove', function(event) {
         // Actualiza las variables sendX y sendY con las coordenadas complejas actuales
         sendX = zAux1.real;
         sendY = zAux1.imaginary;
+        drawRedPoint();
+        drawJuliaSet(juliaComplex, canvasJul, ctxJulia);
     }
 });
 
 // Controlador de eventos para cuando se suelta el botón del mouse
 pointCanvas.on('mouseup', function() {
-    dragging = false;
-    drawJuliaSet(juliaComplex, canvasJul, ctxJulia);
-    
+    redPoint.isDragging = false;
 });
 
 //Función onclick para botón de mandelbrot
@@ -109,14 +93,12 @@ function updateTextBox(c) {
 
 //Función que dibuja el punto rojo en el canvas pointCanvas
 function drawRedPoint(){
-    const pixel = complexToPixel(default_complex, pointCanvas);
-    pixel.real = pixel.real + canvasBorder;
-    pixel.imaginary = pixel.imaginary + canvasBorder;
     ctxPoint.clearRect(0, 0, pointCanvas[0].width, pointCanvas[0].height);
-    ctxPoint.fillStyle = 'red';
     ctxPoint.beginPath();
-    ctxPoint.arc(pixel.real, pixel.imaginary, pointRadius, 0, 2 * Math.PI);
+    ctxPoint.arc(redPoint.x, redPoint.y, redPoint.radius, 0, 2 * Math.PI);
+    ctxPoint.fillStyle = redPoint.color;
     ctxPoint.fill();
+    ctxPoint.closePath();
 }
 
 // Dibujar el conjunto de Mandelbrot al cargar la página
