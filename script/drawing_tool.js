d805358885fb55d5c3a18464c787dc00fe869d12
SamuRodDev <132297110+SamuRodDev@users.noreply.github.com>
2023-06-29 16:25:49 +0200

    [fix]Reestructuración front drawing tool

--- script/drawing_tool.js
+++ script/drawing_tool.js
@@ -2,7 +2,7 @@
 import Complex, { drawJuliaSet, drawMandelbrotSet } from "./fractal_sets_tools.js";
 
 //Lógica para recibir datos de la anterior página
-const labelRec = $("#set_type");
+const labelRec = $("#setType");
 var urlParams = new URLSearchParams(window.location.search);
 var realR = parseFloat(urlParams.get("real"));
 var imagR = parseFloat(urlParams.get("imag"));
@@ -10,15 +10,20 @@ let juliaNumber = new Complex(realR, imagR);
 let juliaAux = new Complex(juliaNumber.real.toFixed(2), juliaNumber.imaginary.toFixed(2)); 
 let setType = 0;
 if ((realR && imagR) || (realR == 0 && imagR ==0)) {
-    labelRec[0].innerText = "Conjunto de Julia correspondiente al número complejo: " + juliaAux.toString();
+    labelRec[0].innerHTML = "Conjunto de Julia correspondiente al número complejo: <b>" + juliaAux.toString() + "</b>";
     setType = 1;
 }
 
 //Variables para el dibujo del conjunto correspondiente
-const canvasDrawing = $("#canvasDrawing");
+const canvasDrawing = $("#canvasSets");
 const ctxDrawing = canvasDrawing.get(0).getContext('2d');
 const default_complex = new Complex(0, 0);
 
 //Dibujamos set de Mandelbrot o set de Julia en función de setType
 if (setType == 0) drawMandelbrotSet(canvasDrawing, ctxDrawing);
 else drawJuliaSet(juliaNumber, canvasDrawing, ctxDrawing);
+
+//Función onclick para botón de mandelbrot
+$("#btnBack").click(function(){
+    window.location.href= "fractal_sets.html";
+})
